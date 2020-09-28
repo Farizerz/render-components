0b31119818dfa51be06589dcfd7c62704e799b44
Farizerz <39519098+Farizerz@users.noreply.github.com>
2020-09-28 10:49:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      items: [],
+      currentItem: {
+        text:'',
+        key:''
+      }
+    }
+  }
+
+  handleInput(e) {
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e) {
+
+  }
+
   render() {
     return (
       <div className="App">
         <header>
-          <form id="to-do-form">
-            <input type="text" placeholder="Enter Text" />
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text" value= {this.state.currentItem.text} onChange={this.handleInput} />  
             <button type="submit">Add</button>
           </form>
         </header>
