a5be8d3bff89989b4a05a309032e477d3c2d4afb
Farizerz <39519098+Farizerz@users.noreply.github.com>
2020-10-01 10:17:00 +0700

    Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import './ListItem';
 import ListItem from './ListItem';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrash);
 
 export default class App extends Component {
   constructor(props) {
@@ -15,7 +19,9 @@ export default class App extends Component {
     }
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
-  }
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  } 
 
   handleInput(e) {
     this.setState({
@@ -29,17 +35,36 @@ export default class App extends Component {
   addItem(e) {
     e.preventDefault(); 
     const newItem = this.state.currentItem;
-    console.log(newItem);
-    if(newItem.text != "") {
+    if(newItem.text !== "") {
       const items=[...this.state.items, newItem];
       this.setState({
-        items: newItem,
+        items: items,
         currentItem:{
           text:'',
           key:''
         }
       })
     }
+  }
+
+  deleteItem(key) {
+    const filterItems = this.state.items.filter( item =>
+      item.key !== key);
+      this.setState({
+        items: filterItems
+      })
+  }
+
+  setUpdate(text, key) {
+    const items = this.state.items;
+    items.map(item => {
+      if(item.key === key) {
+        item.text = text;
+      }
+    })
+    this.setState({
+      items: items
+    })
   }
 
   render() {
@@ -51,7 +76,13 @@ export default class App extends Component {
             <button type="submit">Add</button>
           </form>
         </header>
-        <ListItem items = {this.state.items}></ListItem>
+        <ListItem 
+          items = {this.state.items}
+          deleteItem = {this.deleteItem}
+          setUpdate = {this.setUpdate}
+        >
+        </ListItem>
+
       </div>
     )
   }
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import './ListItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ListItem = (props) => {
     const items = props.items;
     const ListItem = items.map(item => {
-        return <div className="list" key="item.key">
-            <p>{item.text}</p>
+        return <div className="list" key={item.key}>
+            <p>
+                <input 
+                    type="text" 
+                    id={item.key} 
+                    value={item.text} 
+                    onChange = {
+                        (e) => {
+                            props.setUpdate(e.target.value, item.key)
+                        }
+                    }                
+                />
+                <span>
+                    <FontAwesomeIcon 
+                        className="faicons" 
+                        icon="trash" 
+                        onClick= {() => props.deleteItem(item.key)}
+                    />
+                </span>
+            </p>
+
         </div>
     })
     return (
